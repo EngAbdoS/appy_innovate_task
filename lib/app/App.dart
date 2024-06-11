9786d2aea492c7509219a1f9bad6bcaa37d3c6f7
abdelrahman swidan <eng.abdelrahman.swidan@gmail.com>
2024-06-11 12:00:46 +0300

    add main dependencies

---
+++ lib/app/App.dart
@@ -0,0 +1,36 @@
+import 'package:appy_innovate/app/constants.dart';
+import 'package:appy_innovate/presentation/mainPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:loader_overlay/loader_overlay.dart';
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return GlobalLoaderOverlay(
+      child: ScreenUtilInit(
+        designSize: AppConstants.appDesignSize,
+        minTextAdapt: true,
+        splitScreenMode: true,
+
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+
+          ),
+          home: const MainPage(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppConstants{
+
+  static const Size appDesignSize=Size(1920, 1080);
+
+
+
+
+
+
+
+}
